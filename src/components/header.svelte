eb9048f1fd0746558ad095bd8d9b3683a38ce39f
thurkuba <arthur.kubagawa@gmail.com>
2022-10-05 13:53:53 -0300

    comecou, finalmente !

---
+++ src/components/header.svelte
@@ -0,0 +1,12 @@
+<header>
+	<nav>
+		<a href="/home">home</a>
+		<a href="/about">about</a>
+	</nav>
+</header>
+
+<style>
+	nav {
+		background-color: #107200;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from '../components/header.svelte';
+</script>
+
+<div class="envolt">
+	<Header />
+	<h1>teste</h1>
+	<slot><!-- optional fallback --></slot>
+</div>
+
+<style lang="scss">
+	.envolt {
+		background-color: lavender;
+		min-width: 280px;
+		max-width: 800px;
+	}
+</style>
